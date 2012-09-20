d14ea6decd2243391335cb007a4ab64cad3f3648
Guruprasad <dv.guruprasad@gmail.com>
2012-09-20 11:58:37 +0530

    ui stuff

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -7,5 +7,6 @@ class WinesController < ApplicationController
     @wines_with_ratings = {}
     @user.ratings.each {|r| @wines_with_ratings[Wine.find(r.wine_id)] = r.value}
     @recommendations = Recommender.recommend_for(@user)
+    @body_scales = {0 => "Light", 1 => "Medium Light", 2 => "Medium Bodied", 3 => "Medium Full", 4 => "Full Bodied"}
   end
 end
