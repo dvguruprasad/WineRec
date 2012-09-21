48f524ef27525737ebae251b38fe6dcfbf843467
Guruprasad <dv.guruprasad@gmail.com>
2012-09-21 10:06:18 +0530

    first cut at impelementing wine search

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,12 +1,20 @@
 class WinesController < ApplicationController
   def index
+    @body_scales = body_scales
+    @sweetness_scales = {1 => "Dry (Extra Brut)", 2 => "Dry (Brut)", 3 => "Medium Dry(Extra Dry)", 4 => "Medium Dry", 
+                          5 => "Medium(Sec)", 6 => "Medium Sweet", 7 => "Medium Sweet(Demi)", 8 => "Sweet", 9 => "Sweet(Doux)"}
   end
 
   def recommendations
     @user = User.find(params[:id])
-    @wines_with_ratings = {}
-    @user.ratings.each {|r| @wines_with_ratings[Wine.find(r.wine_id)] = r.value}
+    @wines_rated_by_user = {}
+    @user.ratings.each {|r| @wines_rated_by_user[Wine.find(r.wine_id)] = r.value}
     @recommendations = Recommender.recommend_for(@user)
-    @body_scales = {0 => "Light", 1 => "Medium Light", 2 => "Medium Bodied", 3 => "Medium Full", 4 => "Full Bodied"}
+    @body_scales = body_scales
+  end
+
+  private 
+  def body_scales
+    {0 => "Light", 1 => "Medium Light", 2 => "Medium Bodied", 3 => "Medium Full", 4 => "Full Bodied"}
   end
 end
